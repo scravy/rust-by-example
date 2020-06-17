984e338c6ce00deb27b13d4918ae7a984f02d0ed
Julian Fleischer <tirednesscankill@warhog.net>
2020-06-17 18:43:10 +0200

    Use pretty printing

--- 03 Formatted print/format.rs
+++ 03 Formatted print/format.rs
@@ -37,5 +37,5 @@ fn main() {
 
     // However, custom types such as this structure require more complicated
     // handling. This will not work.
-    println!("This struct `{:?}` won't print...", Structure(3));
+    println!("This struct `{:#?}` won't print...", Structure(3));
 }
