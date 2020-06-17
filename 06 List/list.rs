563722bd9601042543d270a1e02ca3c7d44d4f16
Julian Fleischer <tirednesscankill@warhog.net>
2020-06-17 19:14:06 +0200

    Add printing the index too

--- 06 List/list.rs
+++ 06 List/list.rs
@@ -9,7 +9,7 @@ impl fmt::Display for List {
 
         for (count, v) in vec.iter().enumerate() {
             if count != 0 { write!(f, ", ")?; }
-            write!(f, "{}", v)?;
+            write!(f, "{}: {}", count, v)?;
         }
 
         return write!(f, "]");
